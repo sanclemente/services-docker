0a8dd47ee12ada8515d09bf5ef73ebd6dccc708f
javierfp <javierfp@a21eql00.sanclemente.local>
2022-12-14 11:46:28 +0100

    Commit inicial

---
+++ lamp/build/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Arrancamos servicios
+if [ -d /var/lib/mysql ]
+then
+	service mariadb start
+	#Crea la base de datos si no existe
+	mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $DB"
+	#Crea el usuario si no existe y le otorga permisos a la base de datos
+	if [ $(mysql -uroot -D mysql -s -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$DBUSER')") ]
+	then
+		mysql -uroot -e "GRANT ALL ON $DB.* TO '$DBUSER'@'%' IDENTIFIED BY '$DBPASS'"
+	fi
+fi
+
+[ -d /var/www/html ] && service apache2 start
+
+exec bash -c "while true;do sleep 10;done"
+
